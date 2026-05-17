4122d6b0f69ed24a9453b9ea5e315d85d6f3656a
Afiqu3 <gahossainj@gmail.com>
2026-05-17 22:32:24 +0600

    add profile management with create profile functionality and routing

--- src/app.ts
+++ src/app.ts
@@ -4,6 +4,7 @@ import express, {
   type Response,
 } from "express";
 import { userRouter } from "./modules/user/user.route";
+import { profileRouter } from "./modules/profile/profile.route";
 const app: Application = express();
 
 app.use(express.json());
@@ -18,5 +19,6 @@ app.get("/", (req: Request, res: Response) => {
 });
 
 app.use("/api/users", userRouter);
+app.use("/api/profile", profileRouter);
 
 export default app;
